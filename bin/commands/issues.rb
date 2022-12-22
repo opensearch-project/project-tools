f496a4ebc96896477a613452eb1063bd9fdeb57d
Daniel (dB.) Doubrovkine <dblock@dblock.org>
2022-12-22 10:30:20 -0500

    Show number of issues older than 3 months. (#42)
    Signed-off-by: dblock <dblock@amazon.com>
    
    Signed-off-by: dblock <dblock@amazon.com>

--- bin/commands/issues.rb
+++ bin/commands/issues.rb
@@ -30,8 +30,12 @@ module Bin
           untriaged_issues.repos.each_pair do |repo, issues|
             puts "#{repo}: #{issues.count}"
           end
-          puts "\n# Older than 3 Months\n"
-          untriaged_issues.created_before(Time.now - 3.months).repos.each_pair do |repo, issues|
+          older_issues_3mo = untriaged_issues.created_before(Time.now - 3.months)
+          older_issues_3mo_size = older_issues_3mo.repos.map do |_repo, issues|
+            issues.size
+          end.sum
+          puts "\n# Older than 3 Months (#{older_issues_3mo_size})\n"
+          older_issues_3mo.repos.each_pair do |repo, issues|
             puts "#{repo.split('/').last}: #{issues.count}"
           end
           puts "\n# Oldest Issues\n"
