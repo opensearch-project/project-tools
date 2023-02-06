39875dd0c7ed1cca436a4de4a66a3857ab530a79
Daniel (dB.) Doubrovkine <dblock@dblock.org>
2023-02-06 10:02:59 -0500

    Added rate_limited specs. (#46)
    Signed-off-by: dblock <dblock@amazon.com>

--- spec/github/rate_limited_spec.rb
+++ spec/github/rate_limited_spec.rb
@@ -1,5 +1,30 @@
 # frozen_string_literal: true
 
 describe GitHub::RateLimited do
-  pending 'rate_limited'
+  include GitHub::RateLimited
+
+  let(:rate_limit) { Octokit::RateLimit.new }
+
+  before do
+    allow(self).to receive(:sleep)
+    allow(rate_limit).to receive(:resets_in).and_return 0
+    allow($github).to receive(:rate_limit).and_return rate_limit
+  end
+
+  it 'retries on a rate limit' do
+    iteration = 0
+    raised = false
+    calls = []
+    rate_limited do
+      until iteration == 2
+        calls << iteration
+        unless raised
+          raised = true
+          raise Octokit::TooManyRequests
+        end
+        iteration += 1
+      end
+    end
+    expect(calls).to eq [0, 0, 1]
+  end
 end
